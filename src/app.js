f2e7bf606e125204f8f8af7e96a6e6cbc9eea45b
Sonqo-Toconas <Sonqo-Toconas>
2023-07-18 10:58:09 -0300

    Express

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+
 const path = require('path');
 
 app.use(express.static('../public'));
 
 app.listen(3030, () => {
     console.log('Servidor corriendo');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
