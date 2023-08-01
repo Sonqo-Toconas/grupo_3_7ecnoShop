f24911a3d341e6217f1f428d43d456d5f431244a
Sonqo-Toconas <Sonqo-Toconas>
2023-08-01 10:46:39 -0300

    Nodemon

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static('../public'));
+app.use(express.static('public'));
 
 app.listen(3030, () => {
     console.log('Servidor corriendo');
