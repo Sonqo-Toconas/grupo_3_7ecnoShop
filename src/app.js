3a3a64051a69c38d45c0863b97d3b2933e7e846c
RocioRodriguezzz <rocio011005@gmail.com>
2023-08-17 00:56:14 -0300

    parte del ProductDetail

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const registerRoutes = require('./routes/registerRouter');
 const loginRoutes = require('./routes/loginRouter');
 const userPanelRoutes = require('./routes/userPanelRouter');
 const creationRoutes = require('./routes/creationRouter');
+const productDetailRoutes = require('./routes/productDetailRouter');
 
 app.use(express.static('public'));
 const path = require('path')
@@ -17,13 +18,12 @@ app.use('/login', loginRoutes);
 app.use('/registro', registerRoutes);
 app.use('/crear', creationRoutes);
 app.use('/userpanel', userPanelRoutes);
+app.use('/producto', productDetailRoutes);
 
 app.get('/carrito', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/productCart.html'));
 });
-app.get('/producto', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
-});
+
 
 app.listen(3030, () => {
     console.log('Servidor corriendo en el puerto http://localhost:3030');
