345055a5d916cefe3e0e9e94988d36643a33a9b3
Carina-Cruz <karina.bel.cruz.1234@gmail.com>
2023-12-11 01:54:11 -0300

    funcionalidad carrito

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -28,6 +28,7 @@ router.post('/registro', fileUpload.single('imagen'), validationRegister, userCo
 router.get('/login', userControllers.login);
 router.post('/login', validationLogin, userControllers.processLogin);
 router.get('/carrito', middleware, userControllers.carrito);
+router.post('/carrito/:id', userControllers.eliminarDelCarrito);
 router.get('/users', middleware, userControllers.showUsers);
 router.patch('/users/:id', userControllers.changeAdmin);
 router.get('/password', userControllers.password);
@@ -35,7 +36,5 @@ router.patch('/password', userControllers.changePassword);
 router.get('/editar/:id', userControllers.userEdit);
 router.post('/editar/:id', fileUpload.single('image'), userControllers.processUserEdit);
 
-
-//router.post('/agregar-al-carrito/:id', productsControllers.agregarAlCarrito);
 
 module.exports = router;
